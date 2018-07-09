a64a97bee4274dcc17fefd525086f6124ba24645
Douglas Hall <doug@dsdh.uk>
2018-07-09 19:12:54 +0100

    with updates, inc. rounding costs

--- sals-shipping.py
+++ sals-shipping.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 def ground_shipping(weight):
   if weight > 10:
     cost = weight * 4.75
@@ -26,22 +24,27 @@ def drone_shipping(weight):
 def choose_shipping_method(weight):
   ground = ground_shipping(weight)
   drone = drone_shipping(weight)
-  if ground < drone:
-    if ground < premium:
-      cheapest = "Ground"
-      cheapest_cost = ground
-    else:
-      cheapest = "Premium"
-      cheapest_cost = premium
-  else:
+  if (ground < drone) and (ground < premium):
+    cheapest = "Ground"
+    cheapest_cost = ground
+  elif (drone < premium):
     cheapest = "Drone"
     cheapest_cost = drone
-  print ("The cheapest shipping method is " + cheapest + " and the cost is $" + str(cheapest_cost))
+  else:
+    cheapest = "Premium"
+    cheapest_cost = premium
+    cheapest_cost = round(cheapest_cost,2)
+  print("The cheapest shipping method is " + cheapest)
+  print("The cost is ${0:.2f}".format(cheapest_cost))
 
-weight = input("What is the weight of the parcel?")
+        
 premium = 125.00
 
+#Uncomment next two lines to prompt for user input
+#weight = input("What is the weight of the parcel in pounds? ")
+#choose_shipping_method(weight)
 
-print(choose_shipping_method(3.4))
-print(ground_shipping(8.4))
-print(drone_shipping(1.5))
+#Test out choose_shipping_method function with
+#some predefined weights.
+choose_shipping_method(4.8)
+choose_shipping_method(41.5)
